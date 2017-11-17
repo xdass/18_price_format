4767ef1f4c2d646daf4e0f130148cafe76c02f82
xdass <420600@gmail.com>
2017-11-17 15:52:05 +0500

    First attempts to solve task

--- format_price.py
+++ format_price.py
@@ -1,5 +1,36 @@
+from decimal import *
+
+
+# проверка на отрицательное значение
+# проверка на число(не строка)
+# неверный формат числа (123.123.123)
+
+
 def format_price(price):
-    pass
+    try:
+        if isinstance(price, str):
+            price = float(price)
+        if float(price) < 0:
+            raise ValueError
+        if str(price).find('.') != -1:
+            price = str(price)[:str(price).find('.') + 3]
+            return '{:,}'.format(float(price)).replace(',', ' ')
+        else:
+            return '{:,}'.format(price).replace(',', ' ')
+    except ValueError:
+        return None
+
 
 if __name__ == '__main__':
-    pass
+    price_1 = 1231233  # 1 231 233
+    price_2 = 9999.999  # 9 999.99 ??
+    price_3 = 3245.000000  # 3 245
+    price_4 = -40000  # wrong
+    price_5 = 'asdadads'  # wrong
+    price_6 = '123123.12312312.123123'
+    print(format_price(price_1))
+    print(format_price(price_2))
+    print(format_price(price_3))
+    print(format_price(price_4))
+    print(format_price(price_5))
+    print(format_price(price_6))
--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
-# TODO place your tests here
+import unittest
+import format_price as fp
+
+
+class PricetTest(unittest.TestCase):
+    def test_simple_number(self):
+        self.assertEquals(fp.format_price(76521), '76 521')
+
+    def test_with_decimal_point(self):
+        self.assertEquals(fp.format_price(3245.000000), '3 245')
+
+    def test_str_price(self):
+        self.assertEquals(fp.format_price('daassda'), None)
+
+    def test_wrong_number(self):
+        self.assertEquals(fp.format_price('123.123.123'), None)
