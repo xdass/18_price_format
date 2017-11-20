beeebc6e857d2735f577fc294e07f5dfdac26a43
xdass <420600@gmail.com>
2017-11-20 21:57:29 +0500

    Simplify the code

--- format_price.py
+++ format_price.py
@@ -6,12 +6,13 @@ def format_price(price):
         if isinstance(price, str):
             price = float(price)
         if float(price) < 0:
-            raise ValueError
+            return None
         if float(price).is_integer():
             return '{:,.0f}'.format(price).replace(',', ' ')
         else:
-            price = str(price)[:str(price).find('.') + 3]
-            return '{:,}'.format(float(price)).replace(',', ' ')
+            integer, decimal = str(price).split('.')
+            prepared_price = '{}.{}'.format(integer, decimal[:2])
+            return '{:,}'.format(float(prepared_price)).replace(',', ' ')
     except ValueError:
         return None
 
@@ -20,3 +21,19 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         raw_price = sys.argv[1].strip('\'')
         print(format_price(raw_price))
+    price_1 = 1231233  # 1 231 233
+    price_2 = 9999.99999  # 9 999.99 ??
+    price_3 = 3245.000000  # 3 245
+    price_4 = -40000  # wrong
+    price_5 = 'asdadads'  # wrong
+    price_6 = '123123.12312312.123123'
+    price_7 = '12312'
+    price_8 = 5555.3511
+    print(format_price(price_1))
+    print(format_price(price_2))
+    print(format_price(price_3))
+    print(format_price(price_4))
+    print(format_price(price_5))
+    print(format_price(price_6))
+    print(format_price(price_7))
+    print(format_price(price_8))
